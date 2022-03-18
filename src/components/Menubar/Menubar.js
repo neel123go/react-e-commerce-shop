2152405c384cc6039eab8396cf6e9b33e9644929
neel123go <ayounpaul1029@gmail.com>
2022-03-18 22:42:06 +0600

    change website name

--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -5,26 +5,9 @@ import '../Menubar/Menubar.css';
 const Menubar = ({ count }) => {
     return (
         <div>
-            {/* <div className="container my-4">
-                <div className="row">
-                    <div className="col-md-3 logo-container d-flex justify-content-start">
-                        <a href="#"><h3>Faija Akther Faiju</h3></a>
-                    </div>
-                    <div className="col-md-9 menu-container d-flex justify-content-end">
-                        <li><a className='list-item me-5' href="#">HOME</a></li>
-                        <li><a className='list-item me-5' href="#">CART <sup className='cart-count'>{count}</sup></a></li>
-                        <li><a className='list-item me-5' href="#">SHOP</a></li>
-                        <li><a className='list-item' href="#">LOGIN</a></li>
-                    </div>
-                </div>
-            </div> */}
-
-
-
-
             <Navbar bg="light" expand="lg" className='py-3'>
                 <Container>
-                    <Navbar.Brand href="http://localhost:3000/" className='fs-3' style={{ color: 'rgb(80, 80, 80)' }}>Faija Akther Faiju</Navbar.Brand>
+                    <Navbar.Brand href="http://localhost:3000/" className='fs-3' style={{ color: 'rgb(80, 80, 80)' }}>Shoper</Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
